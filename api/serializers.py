f1406c08a461d606b7871cc717a098fa15088e07
MBMishu <masumbillah1717@gmail.com>
2025-05-13 03:48:53 +0600

    update view to store data

---
+++ api/serializers.py
@@ -0,0 +1,214 @@
+from rest_framework import serializers
+from .models import *
+        
+class CountrySerializer(serializers.ModelSerializer):
+    
+    nativeName = serializers.SerializerMethodField()
+    tld = serializers.SerializerMethodField()
+    currency = serializers.SerializerMethodField()
+    
+    idd = serializers.SerializerMethodField()
+    capital = serializers.SerializerMethodField()
+    altSpellings = serializers.SerializerMethodField()
+    languages = serializers.SerializerMethodField()
+    
+    borders = serializers.SerializerMethodField()
+    
+    demonyms = serializers.SerializerMethodField()
+    translations = serializers.SerializerMethodField()
+    gini  = serializers.SerializerMethodField()
+    car = serializers.SerializerMethodField()
+    timezones = serializers.SerializerMethodField()
+    continents = serializers.SerializerMethodField()
+    
+    flags = serializers.SerializerMethodField()
+    coatOfArms = serializers.SerializerMethodField()
+    capitalInfo = serializers.SerializerMethodField()
+    postalCode = serializers.SerializerMethodField()
+    
+    def get_flags(self, obj):
+        flags = obj.flag.all()
+        if not flags:
+            return []
+        result = {}
+        for data in flags:
+            result['png'] = data.png_url
+            result['svg'] = data.svg_url
+            result['alt'] = data.alt_text
+        return result
+    
+    def get_coatOfArms(self, obj):
+        coat_of_arms = obj.coat_of_arms.all()
+        if not coat_of_arms:
+            return []
+        result = {}
+        for data in coat_of_arms:
+            result['png'] = data.png_url
+            result['svg'] = data.svg_url
+        return result
+    
+    def get_capitalInfo(self, obj):
+        capital_info = obj.capital_info.all()
+        if not capital_info:
+            return []
+        result = {}
+        for data in capital_info:
+            result['latlng'] = data.lat , data.lng
+        return result
+    
+    def get_postalCode(self, obj):
+        postal_code = obj.postal_codes.all()
+        if not postal_code:
+            return []
+        result = {}
+        for data in postal_code:
+            result['format'] = data.format
+            result['regex'] = data.regex
+        return result
+    
+    
+    def get_continents(self, obj):
+        continents = obj.continents.all()
+        if not continents:
+            return []
+        return [data.name for data in continents]
+    
+    
+    def get_timezones(self, obj):
+        timezones = obj.timezones.all()
+        if not timezones:
+            return []
+        return [data.name for data in timezones]
+    
+    
+    def get_car(self, obj):
+        car = obj.cars.all()
+        if not car:
+            return []
+        result = {}
+        for data in car:
+            result['side'] = data.side
+            result['signs'] = [sign.sign for sign in data.car_sign.all()]
+        return result
+    
+    def get_gini(self, obj):
+        gini = obj.gini.all()
+        if not gini:
+            return []
+        result = {}
+        for data in gini:
+            result[data.year] = data.value
+        return result
+    
+    def get_translations(self, obj):
+        translations = obj.translations.all()
+        if not translations:
+            return []
+        result = {}
+        for data in translations:
+            result[data.language] = {
+                "official": data.official,
+                "common": data.common
+            }
+        return result
+    
+    def get_demonyms(self, obj):
+        demonym = obj.demonyms.all()
+        if not demonym:
+            return []
+        result = {}
+        for data in demonym:
+            result[data.name] = {
+                "f": data.f,
+                "m": data.m
+            }
+        return result
+    
+    def get_borders(self, obj):
+        borders = obj.borders.all()
+        if not borders:
+            return []
+        
+        return [data.border_country_code for data in borders]
+    
+    def get_languages(self, obj):
+        languages = obj.languages.all()
+        if not languages:
+            return []
+        result = {}
+        for lang in languages:
+            result[lang.code] = lang.name
+        return result
+    
+    def get_altSpellings(self, obj):
+        alt_spellings = obj.alt_spellings.all()
+        if not alt_spellings:
+            return []
+        return [data.alt_spelling for data in alt_spellings]
+    
+    def get_capital(self, obj):
+        capitals = obj.capitals.all()
+        if not capitals:
+            return []
+        return [data.capital for data in capitals]
+    
+    def get_idd(self, obj):
+        idd = obj.idds.all()
+        if not idd:
+            return []
+        result = {}
+        for idd_obj in idd:
+            result['root']  = idd_obj.root
+            result['suffix'] = [suffix.suffix for suffix in idd_obj.suffixes.all()]
+            
+        return result
+    
+    def get_nativeName(self, obj):
+        native_names = obj.native_names.all()
+        result = {}
+        for native in native_names:
+            result[native.name] = {
+                "official": native.official,
+                "common": native.common
+            }
+        return result
+    
+    def get_tld(self, obj):
+        tlds = obj.tlds.all()
+        if not tlds:
+            return []
+        return [tld.domain for tld in tlds]
+    
+    def get_currency(self, obj):
+        currencies = obj.currencies.all()
+        result = {}
+        for currency in currencies:
+            result[currency.code] = {
+                "name": currency.name,
+                "symbol": currency.symbol
+            }
+        return result
+
+    class Meta:
+        model = Country
+        fields = ['name', 'official','nativeName','tld',
+                  'cca2', 'ccn3', 'cioc', 'independent','status','un_member','currency',
+                  'idd','capital','altSpellings','region',
+                  'subregion','languages','latitude','longitude','landlocked','borders','area',
+                  'demonyms','cca3','translations','flag_emoji','google_maps','openstreet_maps','population',
+                  'gini','fifa','car','timezones','continents',
+                  'flags','coatOfArms','startOfWeek','capitalInfo','postalCode'
+                  ]
+        # fields = ['name', 'official','nativeName','tld',
+        #           'cca2', 'cca3', 'cioc', 'independent','status','un_member','currency',
+        #           'idd','capital','alt_spellings_values','region',
+        #           'subregion','languages_value','latitude','longitude','landlocked','borders_value',
+        #           'area','demonym','cca3','translations_value','flag_emoji',
+        #           'google_maps','openstreet_maps','population','gini_value',
+        #           'fifa','car','timezones_value','continent',
+        #           'flag_value','coat_of_arms_value','startOfWeek','capital_info_value','postal_code',
+        #           ]
+        
+
+    
+        
--- api/views.py
+++ api/views.py
@@ -11,7 +11,8 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
-
+from .models import *
+from .serializers import *
 
 @api_view(['GET'])
 def fetch_countries(request):
@@ -23,11 +24,180 @@ def fetch_countries(request):
         for data in countries_data:
             name = data['name']['common']
             
+            country, created = Country.objects.get_or_create(
+                name=name,
+                defaults={
+                    'official': data.get('name', {}).get('official'),
+                    'cca2': data.get('cca2'),
+                    'cca3': data.get('cca3'),
+                    'ccn3': data.get('ccn3'),
+                    'cioc': data.get('cioc'),
+                    'independent': data.get('independent', False),
+                    'status': data.get('status', 'officially-assigned'),
+                    'un_member': data.get('unMember', False),
+                    'region': data.get('region'),
+                    'subregion': data.get('subregion'),
+                    'latitude': data.get('latlng', [None, None])[0],
+                    'longitude': data.get('latlng', [None, None])[1],
+                    'landlocked': data.get('landlocked', False),
+                    'area': data.get('area'),
+                    'flag_emoji': data.get('flag'),
+                    'population': data.get('population'),
+                    'fifa': data.get('fifa'),
+                    'startOfWeek': data.get('startOfWeek'),
+                    'google_maps': data.get('maps', {}).get('googleMaps'),
+                    'openstreet_maps': data.get('maps', {}).get('openStreetMaps'),
+                }
+            )
             
+            print(f"Country: {name}, Created: {created}")
+            
+        
+            for cap in data.get('capital', []):
+                capital.objects.get_or_create(country=country, capital=cap)
+                
+            print(f"Capital: {cap}, Created: {created}")
+                
+            
+            currencies = data.get('currencies', {})
+            for code, currency in currencies.items():
+                Currency.objects.get_or_create(
+                    country=country,
+                    code=code,
+                    name=currency.get('name'),
+                    symbol=currency.get('symbol')
+                )
+                
+            print(f"Currency: {currency}, Created: {created}")
+            
+            # Languages
+            languages = data.get('languages', {})
+            for code, lang in languages.items():
+                Language.objects.get_or_create(country=country, code=code, name=lang)
+                
+            print(f"Language: {lang}, Created: {created}")
+                
+            for border in data.get('borders', []):
+                Border.objects.get_or_create(country=country, border_country_code=border)
+                
+            print(f"Border: {border}, Created: {created}")
+                
+            
+            for tld in data.get('tld', []):
+                TopLevelDomain.objects.get_or_create(country=country, domain=tld)
+                
+            print(f"Top Level Domain: {tld}, Created: {created}")
+                
+            for cont in data.get('continents', []):
+                Continent.objects.get_or_create(country=country, name=cont)
+            
+            print(f"Continent: {cont}, Created: {created}")
+            
+            for time in data.get('timezones', []):
+                Timezone.objects.get_or_create(country=country, name=time)
+            
+            print(f"Continent: {time}, Created: {created}")
+                
+            flags = data.get('flags', {})
+            Flag.objects.get_or_create(
+                country=country,
+                png_url=flags.get('png'),
+                svg_url=flags.get('svg'),
+                alt_text=flags.get('alt')
+            )
+            print(f"Flag: {flags.get('alt')}, Created: {created}")
+            
+            coat = data.get('coatOfArms', {})
+            CoatOfArms.objects.get_or_create(
+                country=country,
+                png_url=coat.get('png'),
+                svg_url=coat.get('svg')
+            )
+            print(f"Coat of Arms: {coat.get('svg')}, Created: {created}")
+            
+            
+            # Native Names
+            
+            native_names = data.get('name', {}).get('nativeName', None)
 
-        return JsonResponse({
-            'countries': countries_data
-        }, status=status.HTTP_200_OK)   
+            if native_names:
+                if isinstance(native_names, dict):
+                    for lang_code, names in native_names.items():
+                        native_name, created = NativeName.objects.get_or_create(country=country, name=lang_code,
+                                                                                   official = names.get('official'),
+                                                                                   common = names.get('common')
+                                                                                   )
+                        
+                        print(f"Native Name: {names.get('official')}, Created: {created}")
+                else:
+                    print(f"Native Name is not a dictionary for country: {name}")
+            else:
+                print(f"No nativeName data found for country: {name}")
+
+                
+            idd_data = data.get('idd', {})
+            if idd_data:
+                idd, _ = Idd.objects.get_or_create(country=country, root=idd_data.get('root'))
+                for suffix_value  in idd_data.get('suffixes', []):
+                    suffix.objects.get_or_create(Idd=idd, suffix=suffix_value )
+            
+            print(f"IDD: {idd_data.get('root')}, Created: {created}")
+            
+            for alt in data.get('altSpellings', []):
+                altSpellings.objects.get_or_create(country=country, alt_spelling=alt)
+                
+            print(f"Alt Spelling: {alt}, Created: {created}")
+            
+            demonyms = data.get('demonyms', {})
+            for  names, values in demonyms.items():
+                demonym, _ = Demonym.objects.get_or_create(country=country, name=names,f = values.get('f'), m = values.get('m'))
+               
+            print(f"Demonym: {names}, Created: {created}")
+            
+            
+            translations = data.get('translations', {})
+            for lang_code, trans in translations.items():
+                translation, _ = Translation.objects.get_or_create(country=country, language=lang_code, 
+                                                                    official=trans.get('official'),
+                                                                    common=trans.get('common'))
+                
+            print(f"Translation: {lang_code}, Created: {created}")
+
+            gini_data = data.get('gini', {})
+            for year, value in gini_data.items():
+                gini, _ = Gini.objects.get_or_create(country=country, year=year, value=value)
+                
+                
+            print(f"Gini: {year}, Created: {created}")
+                
+            car_data = data.get('car', {})
+            if car_data:
+                car, _ = Car.objects.get_or_create(country=country, side=car_data.get('side'))
+                for sign in car_data.get('signs', []):
+                    CarSign.objects.get_or_create(car=car, sign=sign)
+                    
+            print(f"Car: {car_data.get('side')}, Created: {created}")
+                    
+            
+            cap_info = data.get('capitalInfo', {}).get('latlng',[])
+            if cap_info:
+                capitalInfo.objects.get_or_create(country=country, lat = cap_info[0], lng = cap_info[1])
+            else:
+                capitalInfo.objects.get_or_create(country=country, lat = None, lng = None)
+                
+            print(f"Capital Info: {cap_info}, Created: {created}")
+
+            # Postal Code
+            postal = data.get('postalCode', {})
+            if postal:
+                PostalCode.objects.get_or_create(
+                    country=country,
+                    format=postal.get('format'),
+                    regex=postal.get('regex')
+                )
+                
+            print(f"Postal Code: {postal.get('format')}, Created: {created}")
+
+        return JsonResponse({'message': 'Countries fetched and saved successfully'}, status=status.HTTP_200_OK)
     else:
         return JsonResponse({'error': 'Failed to fetch countries'}, status=response.status_code)
-    
