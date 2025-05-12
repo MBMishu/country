80f85dba3a49568ae333bf177f9835dee5991755
MBMishu <masumbillah1717@gmail.com>
2025-05-13 03:45:06 +0600

    fetching data

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path , include
 from django.contrib.auth import views as auth_views
+from .views import *
+from . import views
 
 urlpatterns = [
-    
+   
+    path('fetch-countries/', views.fetch_countries, name='fetch-countries'),
+   
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+
+
+
+@api_view(['GET'])
+def fetch_countries(request):
+    url = 'https://restcountries.com/v3.1/all'
+    response = requests.get(url)
+    if response.status_code == 200:
+        countries_data = response.json()
+ 
+        for data in countries_data:
+            name = data['name']['common']
+            
+            
+
+        return JsonResponse({
+            'countries': countries_data
+        }, status=status.HTTP_200_OK)   
+    else:
+        return JsonResponse({'error': 'Failed to fetch countries'}, status=response.status_code)
+    
